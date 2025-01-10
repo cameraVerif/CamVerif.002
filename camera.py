a7f2d50bdd9ead6ec9349c104041025142473819
habeeb <habeebp@iisc.ac.in>
2025-01-10 15:30:08 +0530

    camera default parameters added

--- camera.py
+++ camera.py
@@ -1,41 +1,9 @@
-
-filmApertureWidth = 0.9872 
-filmApertureHeight = 0.735
-
-imageWidth = 49 #pixels
-imageHeight = 49 #pixels
-
-focalLength = 35
-
-nearClippingPlane = 1
-farClippingPlane = 1000
-
-###########Do not modify#########
-n = nearClippingPlane
-f = farClippingPlane
-
-t =0.35820895522388063
-b =-0.35820895522388063
-l =-0.35820895522388063
-r =0.35820895522388063
-##################################
-
-####1080p camera settings
-
-# filmApertureWidth = 0.9872
-# filmApertureHeight = 0.735
-
-# imageWidth = 1920
-# imageHeight = 1080
-
-# focalLength = 35
-
-# nearClippingPlane = 1
-# farClippingPlane = 1000
-
 #########################################################
 #######################################################
 ##########default paprameters to restore ###################
+#########This is hard coded in some places################
+################Use this if you are runnig full code###########
+#TODO: Make this parameters dynamic
 
 # filmApertureWidth = 0.9872
 # filmApertureHeight = 0.735
@@ -59,3 +27,39 @@ r =0.35820895522388063
 #######################################################
 #####################################################
 ##########################################################
+
+
+filmApertureWidth = 0.9872 
+filmApertureHeight = 0.735
+
+imageWidth = 49 #pixels
+imageHeight = 49 #pixels
+
+focalLength = 35
+
+nearClippingPlane = 1
+farClippingPlane = 1000
+
+###########Do not modify#########
+n = nearClippingPlane
+f = farClippingPlane
+
+t =0.35820895522388063
+b =-0.35820895522388063
+l =-0.35820895522388063
+r =0.35820895522388063
+##################################
+
+####1080p camera settings############
+
+# filmApertureWidth = 0.9872
+# filmApertureHeight = 0.735
+
+# imageWidth = 1920
+# imageHeight = 1080
+
+# focalLength = 35
+
+# nearClippingPlane = 1
+# farClippingPlane = 1000
+#########################################
